d508fbcae6aa9c582b16d798c7d24a41767e43b2
Dharmik Puri Goswami <dharmikpuri2004@gmail.com>
2023-12-03 18:37:27 +0530

    Edit functionality done

--- src/Pages/Fav Page/Fav.jsx
+++ src/Pages/Fav Page/Fav.jsx
@@ -8,6 +8,28 @@ import { useNavigate } from 'react-router-dom';
 const Fav = () => {
   const naviagte = useNavigate();
   const [data, setData] = useState([])
+  const [edit, setEdit] = useState(false);
+  const [editNpm, setEditNpm] = useState({ name: "", id: "" });
+  const handleEdit = (e) => {
+    const val = e.target.value;
+    setEditNpm({ ...editNpm, name: val });
+  }
+
+  const EditNpm = (el) => {
+    setEdit(true);
+    setEditNpm({ name: el.name, id: el.id });
+    
+  }
+
+  const EditSuccess=()=>{
+    axios.patch(`https://sponge-juicy-tub.glitch.me/fav/${editNpm.id}`,editNpm)
+    .then(() => {
+      alert("Edited Successfuly");
+      setEdit(false);
+      getData();
+    })
+  }
+  // console.log(editNpm, "editNpm");
 
   const getData = () => {
     axios.get("https://sponge-juicy-tub.glitch.me/fav")
@@ -21,8 +43,8 @@ const Fav = () => {
         getData();
       })
   }
-  const handleView=(id)=>{
-     naviagte(`/fav/${id}`)
+  const handleView = (id) => {
+    naviagte(`/fav/${id}`)
   }
   useEffect(() => {
     getData();
@@ -43,15 +65,23 @@ const Fav = () => {
               <tr key={i} className='border border-[#4b5c6b]'>
                 <td className='text-[1rem] font-Montserrat font-bold text-black'>{el.name}</td>
                 <td className='flex gap-4 items-center'>
-                  <button className='border border-black rounded-md font-medium  px-4 py-1 text-[1rem] hover:bg-[#41ce41] ' onClick={()=>handleView(el.id)}><FaEye /></button>
+                  <button className='border border-black rounded-md font-medium  px-4 py-1 text-[1rem] hover:bg-[#41ce41] ' onClick={() => handleView(el.id)}><FaEye /></button>
                   <button onClick={() => handleDelete(el.id)} className='border border-black rounded-md font-medium  px-4 py-1 text-[1rem] hover:bg-red-400 ' ><RiDeleteBin6Fill /></button>
-                  <button className='border border-black rounded-md font-medium  px-4 py-1 text-[1rem] hover:bg-[orange] '><FaEdit /></button>
+                  <button className='border border-black rounded-md font-medium  px-4 py-1 text-[1rem] hover:bg-[orange] ' onClick={() => EditNpm(el)}><FaEdit /></button>
                 </td>
               </tr>
             ))}
           </tbody>
         </table>
+
       </div>
+      
+     { edit && <div>
+      <input type="text" placeholder='Edit Npm Package' value={editNpm.name}
+        name='search' className="input input-bordered border-[2px] w-full  mt-1 placeholder:text-[1.3rem] placeholder:font-medium mt-5" onChange={handleEdit} />
+
+      <button className='border mt-4 px-8 py-2 rounded-lg bg-[#6558f5] text-white text-[1.1rem] font-Montserrat font-semibold active:scale-90 transition duration-150 ease-out ' onClick={EditSuccess}>Edit</button>
+      </div> }
     </section >
   )
 }
