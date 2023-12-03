a9bf332366169626179ce7e79eea396c4255817c
Dharmik Puri Goswami <dharmikpuri2004@gmail.com>
2023-12-03 11:31:02 +0530

    Styling part and add fav issue solve

--- src/Pages/Fav Page/Fav.jsx
+++ src/Pages/Fav Page/Fav.jsx
@@ -1,5 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import axios from "axios"
+import { FaEye, FaEdit } from "react-icons/fa";
+import { RiDeleteBin6Fill } from "react-icons/ri";
+
+
 const Fav = () => {
   const [data, setData] = useState([])
 
@@ -10,25 +14,40 @@ const Fav = () => {
   }
   const handleDelete = (id) => {
     axios.delete(`http://localhost:3001/fav/${id}`)
-    .then((res)=> {
-      alert("Deleted Successfuly");
-      getData();
-    })
+      .then(() => {
+        alert("Deleted Successfuly");
+        getData();
+      })
   }
   useEffect(() => {
     getData();
   }, [])
   return (
-    <div>
-      {data.map((el, i) => (
-        <div>
-          <p>{el.name}</p>
-          <button>view</button>
-          <button onClick={()=> handleDelete(el.id)}>Delete</button>
-          <button>Edit</button>
-        </div>
-      ))}
-    </div>
+    <section className='border min-h-screen p-8'>
+      <p className='text-[#4b5c6b] text-[1.5rem] font-Montserrat font-semibold flex items-center justify-between w-full'>Welcome to Favorite NPM Packages <button className='border-2 rounded-md px-4 py-1 bg-[#6558f5] text-white text-[1.2rem]'>Add fav</button></p>
+      <div className='border-2 border-[#4b5c6b] mt-4 overflow-x-auto'>
+        <table className="table border collapse">
+          <thead>
+            <tr className='border border-[#4b5c6b]'>
+              <th className='text-black font-Montserrat font-bold text-[1.1rem]'>Pazckage Name</th>
+              <th className='text-black font-Montserrat font-bold text-[1.1rem]'>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((el, i) => (
+              <tr key={i} className='border border-[#4b5c6b]'>
+                <td className='text-[1rem] font-Montserrat font-bold text-black'>{el.name}</td>
+                <td className='flex gap-4 items-center'>
+                  <button className='border border-black rounded-md font-medium  px-4 py-1 text-[1rem] hover:bg-[#41ce41] '><FaEye /></button>
+                  <button onClick={() => handleDelete(el.id)} className='border border-black rounded-md font-medium  px-4 py-1 text-[1rem] hover:bg-red-400 '><RiDeleteBin6Fill /></button>
+                  <button className='border border-black rounded-md font-medium  px-4 py-1 text-[1rem] hover:bg-[orange] '><FaEdit /></button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </section >
   )
 }
 
--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from "axios";
 import { FaStar } from "react-icons/fa6";
 const Home = () => {
+  const [data, setData] = useState([])
   const [allpackage, setAllpackage] = useState("");
   const [search, setSearch] = useState("");
   const [searchItem, setSearchItem] = useState(JSON.parse(localStorage.getItem("search")) || []);
@@ -12,35 +13,51 @@ const Home = () => {
     // console.log(val);
   }
   const handleSearchBtn = () => {
-    const filterItem = allpackage.filter((el, i) => el.name.toLowerCase().includes(search.toLowerCase()));
+    const filterItem = allpackage.filter((el) => el.name.toLowerCase().includes(search.toLowerCase()));
     // console.log(filterItem, "Filter Item");
     localStorage.setItem("search", JSON.stringify(filterItem));
     setSearchItem(filterItem);
 
   }
+  // useEffect(() => {
   const AddToFav = (el) => {
-    axios.post("http://localhost:3001/fav", el)
-    .then((res)=> alert(`${el.name} added to favourate`))
-    .catch((err) => console.log(err.message));
+    const verification = data.some((item) => (
+      item.id == el.id
+    ))
+    { !verification ? axios.post("http://localhost:3001/fav", el).then(() => alert("Aded to fav")).catch((err) => console.log(err.message)) : alert("Already hai bhai") }
   }
+  // }, []);
   useEffect(() => {
     axios.get("http://localhost:3001/favnpm")
       .then((res) => setAllpackage(res.data))
       .catch((err) => console.log(err));
+
+    axios.get("http://localhost:3001/fav")
+      .then((res) => setData(res.data))
+      .catch((err) => console.log(err))
+
   }, [])
   return (
-    <div>
-      <label>Search for NPM Packages</label> <br />
-      <input type='text' placeholder='Search Npm Packages' value={search}
-        name='search' onChange={handleSearch} /> <br />
-      <button onClick={handleSearchBtn}>Search</button>
+    <section className='border min-h-screen px-8 py-8'>
       <div>
-        {searchItem.map((el, i) => {
-          return <p>{el.name}<label style={{ marginLeft: "20px" }} onClick={() => AddToFav(el)}>{<FaStar style={{ color: "#FFEB3B" }} />}</label></p>
-        })}
+        <label className='text-[#4b5c6b] text-[1.5rem] font-Montserrat font-semibold'>Search for NPM Packages</label>
+        <br />
+        <input type="text" placeholder='Search Npm Packages' value={search}
+          name='search' onChange={handleSearch} className="input input-bordered border-[2px] w-full  mt-1 placeholder:text-[1.3rem] placeholder:font-medium" />
+        <br />
+        <button className='border mt-4 px-8 py-2 rounded-lg bg-[#6558f5] text-white text-[1.1rem] font-Montserrat font-semibold active:scale-90 transition duration-150 ease-out ' onClick={handleSearchBtn}>Search</button>
+        <div className='mt-4'>
+          {searchItem.map((el, i) => {
+            return <p key={i} className='flex items-center text-[1.5rem] font-Montserrat font-medium'>
+              {el.name}
+              <label style={{ marginLeft: "20px" }} onClick={() => AddToFav(el)}>
+                {<FaStar className='hover:text-[lightgreen] active:scale-90 transition duration-150 ease-out text-[#FFEB3B] cursor-pointer' />}
+              </label>
+            </p>
+          })}
+        </div>
       </div>
-
-    </div>
+    </section>
   )
 }
 
